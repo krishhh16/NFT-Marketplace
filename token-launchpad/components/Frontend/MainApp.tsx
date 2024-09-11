83b72a1ab2dcd64c2755b28d6b54906fa250bf5d
krishhh16 <krishh16@yahoo.com>
2024-09-11 22:43:57 +0530

    finally..... done with the project

--- token-launchpad/components/Frontend/MainApp.tsx
+++ token-launchpad/components/Frontend/MainApp.tsx
@@ -5,10 +5,10 @@ import { Input } from '../ui/input'
 import { Label } from '../ui/label'
 import { Button } from '../ui/button'
 import { toast, Toaster } from "sonner"
-import { clusterApiUrl, Connection, Keypair, SystemProgram, Transaction } from "@solana/web3.js";
+import { clusterApiUrl,PublicKey, Connection, Keypair, LAMPORTS_PER_SOL, SystemProgram, Transaction } from "@solana/web3.js";
 import { useConnection, useWallet } from '@solana/wallet-adapter-react';
 import { MINT_SIZE, TOKEN_2022_PROGRAM_ID, createMintToInstruction, createAssociatedTokenAccountInstruction, getMintLen, createInitializeMetadataPointerInstruction, createInitializeMintInstruction, TYPE_SIZE, LENGTH_SIZE, ExtensionType, mintTo, getOrCreateAssociatedTokenAccount, getAssociatedTokenAddressSync } from "@solana/spl-token"
-import { createInitializeInstruction, pack } from '@solana/spl-token-metadata';
+import { createInitializeInstruction, pack, TokenMetadata } from '@solana/spl-token-metadata';
 
 const formData = z.object({
     uri: z.string().url({ message: "Please input a valid url" }),
@@ -18,83 +18,99 @@ const formData = z.object({
 })
 
 function MainApp() {
-    const [form, setFormData] = useState<z.infer<typeof formData>>()
+    const [mintData, setMintData] = useState<{mintAcc: String, associateTokenData: string}>({
+        mintAcc: "",
+        associateTokenData: ""
+    })
+    const [minted, setMinted] = useState(false)
+    const [form, setFormData] = useState<z.infer<typeof formData>>({
+        uri: "https://github.com",
+        name: "Zeref",
+        symbol: "ZRF",
+        decimal: 9
+    })
     const connection = new Connection(clusterApiUrl("devnet"))
     const wallet = useWallet();
 
     async function createToken() {
         if (wallet.publicKey){
+            
+            try {
             const mintKeypair = Keypair.generate();
-            const metadata = {
+            const myKey = wallet.publicKey;
+            const metadata: TokenMetadata = {
+                name: form?.name as string,
+                uri: form?.uri as string,
+                symbol: form?.symbol as string,
                 mint: mintKeypair.publicKey,
-                name: 'KIRA',
-                symbol: 'KIR    ',
-                uri: 'https://cdn.100xdevs.com/metadata.json',
-                additionalMetadata: [],
-            };
-    
-            const mintLen = getMintLen([ExtensionType.MetadataPointer]);
+                additionalMetadata: []
+            }
+
+            console.log("Metadata: ",   metadata.name)
+            console.log("Form: ", form)
+            console.log(mintKeypair.publicKey.toString())
+
+            const mintLen = getMintLen([ExtensionType.MetadataPointer])
             const metadataLen = TYPE_SIZE + LENGTH_SIZE + pack(metadata).length;
-    
-            const lamports = await connection.getMinimumBalanceForRentExemption(mintLen + metadataLen);
-    
+
+            const lamports = await connection.getMinimumBalanceForRentExemption(mintLen + metadataLen)
+            console.log("I only run till here")
             const transaction = new Transaction().add(
                 SystemProgram.createAccount({
-                    fromPubkey: wallet.publicKey,
-                    newAccountPubkey: mintKeypair.publicKey,
-                    space: mintLen,
+                    fromPubkey: myKey,
                     lamports,
+                    newAccountPubkey: mintKeypair.publicKey,
                     programId: TOKEN_2022_PROGRAM_ID,
+                    space: mintLen
                 }),
                 createInitializeMetadataPointerInstruction(mintKeypair.publicKey, wallet.publicKey, mintKeypair.publicKey, TOKEN_2022_PROGRAM_ID),
-                createInitializeMintInstruction(mintKeypair.publicKey, 9, wallet.publicKey, null, TOKEN_2022_PROGRAM_ID),
+                createInitializeMintInstruction(mintKeypair.publicKey, form?.decimal as number, wallet.publicKey, null, TOKEN_2022_PROGRAM_ID),
                 createInitializeInstruction({
-                    programId: TOKEN_2022_PROGRAM_ID,
-                    mint: mintKeypair.publicKey,
                     metadata: mintKeypair.publicKey,
-                    name: metadata.name,
-                    symbol: metadata.symbol,
                     uri: metadata.uri,
-                    mintAuthority: wallet.publicKey,
-                    updateAuthority: wallet.publicKey,
-                }),
-            );
-                
-            transaction.feePayer = wallet.publicKey;
+                    name: metadata.name,
+                    mintAuthority: myKey,
+                    mint: mintKeypair.publicKey,
+                    symbol: metadata.symbol,
+                    programId: TOKEN_2022_PROGRAM_ID,
+                    updateAuthority: wallet.publicKey
+                })
+            )
+            
+            transaction.feePayer = myKey;
             transaction.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
-            transaction.partialSign(mintKeypair);
-    
-            await wallet.sendTransaction(transaction, connection);
-    
-            console.log(`Token mint created at ${mintKeypair.publicKey.toBase58()}`);
-            const associatedToken = getAssociatedTokenAddressSync(
-                mintKeypair.publicKey,
-                wallet.publicKey,
-                false,
-                TOKEN_2022_PROGRAM_ID,
-            );
-    
-            console.log(associatedToken.toBase58());
-    
+            transaction.partialSign(mintKeypair)
+            
+            
+            await wallet.sendTransaction(transaction, connection) 
+            console.log("but not till here")
+            const associateTokenAccount = getAssociatedTokenAddressSync(
+                mintKeypair.publicKey,myKey, false, TOKEN_2022_PROGRAM_ID
+            )
+
             const transaction2 = new Transaction().add(
                 createAssociatedTokenAccountInstruction(
-                    wallet.publicKey,
-                    associatedToken,
-                    wallet.publicKey,
-                    mintKeypair.publicKey,
-                    TOKEN_2022_PROGRAM_ID,
-                ),
-            );
-    
-            await wallet.sendTransaction(transaction2, connection);
-    
+                    wallet.publicKey, associateTokenAccount, myKey, mintKeypair.publicKey, TOKEN_2022_PROGRAM_ID
+                )
+            )
+
+            await wallet.sendTransaction(transaction2, connection)
+
             const transaction3 = new Transaction().add(
-                createMintToInstruction(mintKeypair.publicKey, associatedToken, wallet.publicKey, 1000000000, [], TOKEN_2022_PROGRAM_ID)
-            );
-    
-            await wallet.sendTransaction(transaction3, connection);
-    
-            console.log("Minted!")
+                createMintToInstruction(mintKeypair.publicKey, associateTokenAccount, wallet.publicKey, 1000000000, [], TOKEN_2022_PROGRAM_ID)
+            )
+
+            await wallet.sendTransaction(transaction3, connection)
+
+            setMinted(true)
+            setMintData({
+                mintAcc: mintKeypair.publicKey.toString(),
+                associateTokenData: associateTokenAccount.toString()
+            })
+
+            }catch (err: any) {
+                toast(`Error happened: ${err.message}`)
+            }
         }
         else{
             toast("Please Connect to the wallet first")
@@ -110,6 +126,8 @@ function MainApp() {
             toast(JSON.parse(parsedForm.error.message)[0].message)
             return
         }
+
+        createToken()
 
     }
 
@@ -182,9 +200,17 @@ function MainApp() {
                     <Button disabled={!wallet.publicKey} type="submit" className="w-[80%] mx-auto" variant={"secondary"}>
                         Make Token
                     </Button>
+                    {
+                        minted &&
+                        <div>
+                            <Label>Your Mint is: {mintData.mintAcc}</Label>
+                            <Label>Your ATA is: {mintData.associateTokenData}</Label>
+                        </div>
+                    }
                 </form>
+
             </div>
-            <Toaster/>
+            <Toaster theme="system" />
         </div>
     )
 }
